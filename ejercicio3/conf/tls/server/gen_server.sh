5852414cccdb30e6cfa13c456ded94caacfbb315
Miquel <miquelrc95@outlook.es>
2020-11-20 19:11:19 +0100

    fixes gen_server script

--- ejercicio3/conf/tls/server/gen_server.sh
+++ ejercicio3/conf/tls/server/gen_server.sh
@@ -3,3 +3,5 @@
 openssl genrsa -out broker.key 2048
 
 openssl req -out broker.csr -key broker.key -new
+
+openssl x509 -req -in server.csr -CA ../ca/ca.crt -CAkey ../ca/ca.key -CAcreateserial -out server.crt -days 365
