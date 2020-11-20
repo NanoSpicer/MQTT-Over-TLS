9486f2076f51bc0dc709982322f6a7ff1d05859d
Miquel <miquelrc95@outlook.es>
2020-11-20 19:22:17 +0100

    regenerar material cliente

--- ejercicio3/conf/tls/client/gen_client.sh
+++ ejercicio3/conf/tls/client/gen_client.sh
@@ -3,3 +3,5 @@
 openssl genrsa -out client.key 2048
 
 openssl req -out client.csr -key client.key -new
+
+openssl x509 -req -in client.csr -CA ../ca/ca.crt -CAkey ../ca/ca.key -CAcreateserial -out client.crt -days 365
