d72a50e39d6dbf9bf0ffacc1e63ebcc74f1033ce
Miquel <miquelrc95@outlook.es>
2020-12-03 19:22:48 +0100

    fix config files

--- ejercicio3/conf/tls/server/gen_server.sh
+++ ejercicio3/conf/tls/server/gen_server.sh
@@ -4,4 +4,4 @@ openssl genrsa -out broker.key 2048
 
 openssl req -out broker.csr -key broker.key -new
 
-openssl x509 -req -in server.csr -CA ../ca/ca.crt -CAkey ../ca/ca.key -CAcreateserial -out server.crt -days 365
+openssl x509 -req -in broker.csr -CA ../ca/ca.crt -CAkey ../ca/ca.key -CAcreateserial -out broker.crt -days 365
